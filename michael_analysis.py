ea5d7668df4c50e9f139bb0c95f09ab1aafa3de4
Jordan Guerguiev <jordang@fastmail.com>
2017-05-23 21:42:32 -0400

    Clean up Michael's analysis file

--- michael_analysis.py
+++ michael_analysis.py
@@ -4,36 +4,46 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-# OIR Files - 0.67 s / frame
+# --- PLOTTING TAIL ANGLES --- #
 
-tracking_path = "Michael's Videos/may.19.17 fish3 loom lv30 0.3 10s_tracking.npz"
-# tracking_path = "test/May.19.17 day2Fish loom lv30 0.3 at 10s_tracking.npz"
-# tracking_path = "test/May.19.17 day2fish deep lv30 0.3 escape 3_tracking.npz"
-video_path = "Michael's Videos/may.19.17 fish3 loom lv30 0.3 10s.avi"
-behavior_times = [4160]
+# Set the base filename here
+fname = "may.19.17 fish3 loom lv30 0.3 10s"
 
-scope_video_n_frames = 25
+# Set the paths to the tracking data & the fish video
+tracking_path = "Michael's Videos/{}_tracking.npz".format(fname)
+video_path = "Michael's Videos/{}.avi".format(fname)
 
+# Get info about fish video
 fps, n_frames = tracking.get_video_info(video_path)
-
 print("FPS: {}, # frames: {}.".format(fps, n_frames))
 
+# Load up tracking data
 (tail_coords_array, spline_coords_array,
  heading_angle_array, body_position_array,
  eye_coords_array, tracking_params) = an.open_saved_data(tracking_path)
 
-# calculate tail angles
+# Get tail angle array
 tail_angle_array = an.get_headfixed_tail_angles(tail_coords_array, tail_angle=tracking_params['tail_angle'], tail_direction=tracking_params['tail_direction'])
-
 tail_end_angle_array = an.get_tail_end_angles(tail_angle_array, num_to_average=3)[0]
 
-plt.figure(figsize=(8, 4))
+# Plot & save the tail angle array
+plt.figure(figsize=(20, 4))
 plt.plot(tail_end_angle_array, 'b', lw=0.5)
-plt.savefig("{}_tail_angle.png".format(os.path.splitext(os.path.basename(video_path))[0]), dpi=200)
-plt.savefig("{}_tail_angle.svg".format(os.path.splitext(os.path.basename(video_path))[0]), dpi=200)
+plt.show()
+plt.savefig("Michael's Videos/{}_tail_angle.png".format(os.path.splitext(os.path.basename(video_path))[0]), dpi=200)
+plt.savefig("Michael's Videos/{}_tail_angle.svg".format(os.path.splitext(os.path.basename(video_path))[0]), dpi=200)
+np.savetxt("Michael's Videos/{}_tail_angle.csv".format(os.path.splitext(os.path.basename(video_path))[0]), tail_end_angle_array, delimiter=",")
 
-# plt.show()
+# --- CONVERTING BEHAVIOR TIMES TO CORRESPONDING TIMES IN SCOPE VIDEO --- #
+# OIR Files - 0.67 s / frame
 
-behavior_times_converted = [ scope_video_n_frames*behavior_time/tail_end_angle_array.shape[-1] for behavior_time in behavior_times ]
+# Set scope frames per second
+scope_video_fps = 1.0/0.67
+
+# List of frame #(s) where behavior event(s) begin
+behavior_times = [4160]
+
+# Convert these to corresponding frame #s in the scope video
+behavior_times_converted = [ ((behavior_time/fps)*scope_video_fps) for behavior_time in behavior_times ]
 
 print(behavior_times_converted)
