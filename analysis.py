f3c7d622e7a8c954d4da63e4fd31cc7ff022f69e
Michael Martin <michaelpr.martin@mail.utoronto.ca>
2017-11-08 13:39:39 -0500

    changed axis for plotting headfixed

--- analysis.py
+++ analysis.py
@@ -87,15 +87,18 @@ def calculate_tail_angles(heading_angle, tail_coords_array):
 def calculate_tail_end_angles(tail_angle_array, num_to_average=1, plot=False):
     n_frames = tail_angle_array.shape[0]
     tail_end_angles = np.zeros(n_frames)
+    time_for_graph = []
 
     for i in range(n_frames):
         tail_angles = tail_angle_array[i, :]
         tail_end_angles[i] = np.mean(tail_angles[ ~np.isnan(tail_angles) ][-num_to_average:])
+        time_for_graph.append(float(i)/350)
 
     if plot:
-        plt.plot(tail_end_angles, 'r', lw=1)
+        plt.plot(time_for_graph, tail_end_angles,'r', lw=1)
+##        plt.plot(time_for_graph, time_for_graph, 'b', lw=1)#Second graph for activity
         plt.title("Tail end angles (averaged over last {} point{} along the tail)".format(num_to_average, "s"*(num_to_average > 1)))
-        plt.xlabel("Frame #")
+        plt.xlabel("Time (Seconds)")
         plt.ylabel("Angle (Degrees)")
         plt.show()
 
--- headfixed_tail_plotting.py
+++ headfixed_tail_plotting.py
@@ -5,7 +5,13 @@ import scipy.stats
 import os
 import json
 
-csv_path = "../testing-nov6B/OMR behaviour 350fps to 30fps_tail_angles.csv"
+def getStimParams ():
+    stimType = input("Please type a single number to select the stimulus.\
+1 for moving dot\
+2 for loom\
+3 for OMR")
+
+csv_path = "../testing-nov6B/Oct.11.17_1B_prey_tail_angles.csv"
 
 tail_angle_array = analysis.load_tail_angles(csv_path)
 
